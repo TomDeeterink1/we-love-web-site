5b87d951d64f7d530f4536b504030262b34ca129
TomDeeterink1 <deeterinktom@gmail.com>
2024-05-21 11:40:36 +0200

    large push with detail page

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ buttons.forEach(button => {
 
 function clicker() {
     gsap.to(window, { duration: 0.8, scrollTo: "#s2home" });
-    gsap.from('.intro-text-2', { duration: 1.1, x: '-50vw', ease: 'power1.out'})
+    gsap.from('.intro-text-2', { duration: 1, y: '8vh', ease: 'power1.out'})
+    gsap.from('.grid2', { duration: 1.1, y: '8vh', ease: 'power1.out'});
 }
 
 const goupper = document.querySelector(".go-up");
@@ -16,10 +17,19 @@ const goupper = document.querySelector(".go-up");
 goupper.addEventListener("click" , letsgoup)
 
 function letsgoup() {
-    gsap.to(window, { duration: 1.2, scrollTo: 0, delay: 0.2, ease: "back.out(1.7)" });
+    gsap.to(window, { duration: 1.1, scrollTo: 0, delay: 0, ease: "power1.out" });
     
 }
 
 
+const button = document.querySelectorAll(".next-button");
 
+button.forEach(button => {
+    button.addEventListener("click", clickers);
+});
+
+function clickers() {
+    gsap.to(window, { duration: 0.8, scrollTo: "#s3home" });
+    gsap.from('.grid', { duration: 1.1, y: '8vh', ease: 'power1.out'});
+}
 
